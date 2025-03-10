94e1e2fd2e1635c2752cbcdd465302f72c77e03c
Deva658 <devaaditya271@gmail.com>
2025-03-10 16:26:01 +0700

    tambahkan fungsi dan if else

---
+++ FUNGSIDANPROSEDUR.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+double alas, tinggi;
+
+void prosedureinput(){
+    cout << "Masukan NIlai Alas : ";
+    cin >> alas;
+
+    cout << "Masukan Nilai Tinggi : ";
+    cin >> tinggi;
+}
+
+double hitungluas(){
+    return 0.5 * alas * tinggi;
+}
+
+void proseduroutput(){
+    cout << "Luas Segitiga = " << hitungluas() << endl;
+}
+
+int main(){
+    prosedureinput();
+    proseduroutput();
+}
